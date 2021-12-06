02b80640754017b1926327a2e0f0ce75982fb5e1
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-06 19:32:47 -0300

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+import SigninPage from "./pages/signin";
 
 function App() {
   return (
     <div className="App">
       <p>Calculadora</p>
+      <SigninPage />
     </div>
   );
 }
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,2 +1,7 @@
 import React from "react";
 
+const Signin: React.FC = () => {
+    return <div />;
+}
+
+export default Signin;
